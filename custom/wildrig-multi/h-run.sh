96aa9994e0d3e45003a8aaa43ce3bfdbbd62d931
HaloGenius <halogenius@yandex.ru>
2018-10-25 21:36:58 +0300

    add libs and fix possible socket error during miner restart

--- custom/wildrig-multi/h-run.sh
+++ custom/wildrig-multi/h-run.sh
@@ -2,6 +2,8 @@
 
 cd `dirname $0`
 
+export LD_LIBRARY_PATH=.
+
 [ -t 1 ] && . colors
 
 . h-manifest.conf
@@ -11,5 +13,10 @@ cd `dirname $0`
 [[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1
 CUSTOM_LOG_BASEDIR=`dirname "$CUSTOM_LOG_BASENAME"`
 [[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
+
+#try to release TIME_WAIT sockets
+for con in `netstat -anp | grep TIME_WAIT | grep ${API_SOCKET} | awk '{print $5}' `; do
+    killcx $con lo
+done
 
 ./wildrig-multi $(< /hive/custom/$CUSTOM_NAME/$CUSTOM_NAME.conf) $@ 2>&1 | tee $CUSTOM_LOG_BASENAME.log
